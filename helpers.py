eda3ac30603f70ed964b85cf1d88906d2970ad31
alif <ali.fawad@readyeducation.com>
2020-04-19 02:04:44 +0500

    add youtube_dl :

--- helpers.py
+++
@@ -1,2 +0,0 @@
-def sanitize_input(text):
-  return text
--- ytdl_web.py
+++ ytdl_web.py
@@ -1,24 +1,37 @@
-from flask import Flask, request, redirect, url_for, render_template
-from youtube_dl import YoutubeDL
-from helpers import sanitize_input
+"""
+This module runs a flask web server as a web interface for youtube-dl
+"""
 
+from os import getenv
+from urllib.parse import urlparse
+from flask import Flask, request, redirect, url_for, render_template, flash
+from dotenv import load_dotenv
+from ytdl_wrapper import download_yt
+
+load_dotenv()
 app = Flask(__name__)
+app.secret_key = getenv('SECRET')
 
 @app.route('/', methods=['GET'])
 def index():
+    """
+    return the index page
+    """
     return render_template('index.html')
 
 @app.route('/download', methods=['POST'])
 def download():
+    """
+    process and download the incoming url
+    """
     if request.method == 'POST':
-        text = request.form['url-text']
+        raw_url = request.form['url-text']
+        parsed_url = urlparse(raw_url)
+        if parsed_url.hostname != "www.youtube.com":
+            flash('Only URLs from Youtube allowed (for now)')
+            return redirect(url_for('index'))
 
-        print(text)
+        download_yt(parsed_url)
         return redirect(url_for('index'))
 
-if __name__ == '__main__':
-    app.run(
-        host='0.0.0.0',
-        port=5000,
-        debug=True
-    )
+# https://www.youtube.com/watch?v=glaG64Ao7sM
